d7f61f099341e17f3e1bf23cea0d9b4b536ceb39
feron <avel.feron@gmail.com>
2020-11-13 16:38:37 +0100

    suppersion preset

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
-import { preset } from 'vue-cli-plugin-vuetify-preset-reply'
+
 Vue.use(Vuetify)
 
 const opts = {
     icons: {
         iconfont: 'mdi', // default - only for display purposes
-    },
-    preset
+    }
 }
 
 export default new Vuetify(opts)
